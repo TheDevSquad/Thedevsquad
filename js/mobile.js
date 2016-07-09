104a0263aae1bfee514c32a3fa4411939bb89fa9
Wilson Wang <xerodragon72@thedevsquad.tech>
2016-07-09 13:20:40 -0400

    adding mobile acceptance
    wow this is a lot of work

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,6 @@
 <script type="text/javascript">
   if (screen.width <= 800) {
-    window.location = "mobile.html";
-  }
-</script>
+    var xx = confirm("You are using a mobile device.\nPress Cancel to view the Full Desktop site.");
+    if (xx == true) {    x = window.location = "http://clwalker.ddns.net/mobile/index.html";}
+      else {    x = window.location = "http://clwalker.ddns.net/index2.html";}
+} </script>
