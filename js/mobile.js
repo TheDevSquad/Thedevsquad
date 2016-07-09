532d64445271878a698bb24f231cb60e3392d5c8
Wilson Wang <xerodragon72@thedevsquad.tech>
2016-07-09 13:24:12 -0400

    inporing mobile support
    yeah

--- js/mobile.js
+++ js/mobile.js
@@ -1,6 +1,1 @@
-<script type="text/javascript">
-  if (screen.width <= 800) {
-    var xx = confirm("You are using a mobile device.\nPress Cancel to view the Full Desktop site.");
-    if (xx == true) {    x = window.location = "http://clwalker.ddns.net/mobile/index.html";}
-      else {    x = window.location = "http://clwalker.ddns.net/index2.html";}
-} </script>
+//deprecated
